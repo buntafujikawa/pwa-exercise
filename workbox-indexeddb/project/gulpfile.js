3c1bf2c646a6608071df7a250da9e9b178b88aba
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-06-19 21:03:40 +0900

    complete Cache the app shell

--- workbox-indexeddb/project/gulpfile.js
+++ workbox-indexeddb/project/gulpfile.js
@@ -18,6 +18,7 @@ limitations under the License.
 const gulp = require('gulp');
 const del = require('del');
 const runSequence = require('run-sequence');
+const workboxBuild = require('workbox-build');
 
 // this task removes old files
 gulp.task('clean', () => del(['.tmp', 'build/*', '!build/.git'], {dot: true}));
@@ -41,3 +42,30 @@ gulp.task('default', ['clean'], cb => {
 gulp.task('watch', function() {
   gulp.watch('app/**/*', ['default']);
 });
+
+gulp.task('service-worker', () => {
+  return workboxBuild.injectManifest({
+    swSrc: 'app/sw.js',
+    swDest: 'build/sw.js',
+    globDirectory: 'build',
+    globPatterns: [
+      'style/main.css',
+      'index.html',
+      'js/idb-promised.js',
+      'js/main.js',
+      'images/**/*.*',
+      'manifest.json'
+    ]
+  }).catch(err => {
+    console.log('[ERROR]: ' + err);
+  });
+});
+
+gulp.task('default', ['clean'], cb => {
+  runSequence(
+    'copy',
+    'service-worker',
+    cb
+  );
+});
+
