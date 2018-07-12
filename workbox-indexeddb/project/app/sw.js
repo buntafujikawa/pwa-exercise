0a2f038113e77f4c575b45af87fe7e492ae08f41
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-07-12 23:17:33 +0900

    complete 8. (Optional) Use workbox-background-sync

--- workbox-indexeddb/project/app/sw.js
+++ workbox-indexeddb/project/app/sw.js
@@ -21,3 +21,29 @@ if (workbox) {
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
+
+const showNotification = () => {
+  self.registration.showNotification('Background sync success!', {
+    body: '🎉`🎉`🎉`'
+  });
+};
+
+const bgSyncPlugin = new workbox.backgroundSync.Plugin(
+  'dashboardr-queue',
+  {
+    callbacks: {
+      queueDidReplay: showNotification
+      // other types of callbacks could go here
+    }
+  }
+);
+
+const networkWithBackgroundSync = new workbox.strategies.NetworkOnly({
+  plugins: [bgSyncPlugin],
+});
+
+workbox.routing.registerRoute(
+  /\/api\/add/,
+  networkWithBackgroundSync,
+  'POST'
+);
