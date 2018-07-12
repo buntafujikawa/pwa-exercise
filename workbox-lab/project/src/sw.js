c7fc61c9023b05bd708665034fcb12ce426a0492
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-07-13 08:53:42 +0900

    complete 8. Add routes to the service worker

--- workbox-lab/project/src/sw.js
+++ workbox-lab/project/src/sw.js
@@ -17,9 +17,20 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
-
   workbox.precaching.precacheAndRoute([]);
-
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
+
+workbox.routing.registerRoute(
+  /(.*)articles(.*)\.(?:png|gif|jpg)/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'images-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      })
+    ]
+  })
+);
